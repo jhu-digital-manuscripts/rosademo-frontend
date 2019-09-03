6053dd282374f756c165fe13a2893116470b1026
GroovinChip <groovinchip@gmail.com>
2019-09-03 14:36:24 -0400

    initial commit

--- src/plugin.js
+++ src/plugin.js
@@ -1,13 +1,65 @@
 import React from "react";
 
-const DemoComponent = props => {
-  console.log(props);
-  return <div id="add-plugin-component-anything">Hello World!</div>;
-};
+class DemoComponent extends React.Component {
+  constructor(props) {
+    // For use later
+    super(props);
+    // Set the initial state
+    this.state = { responseText: "" };
+    // Bind the request to this
+    this.sendRequest = this.sendRequest.bind(this);
+  }
+
+  componentDidMount() {
+    // send the request on load
+    this.sendRequest();
+  }
+
+  sendRequest() {
+    // Bound to `this` to make sure that we dont lose class context
+    fetch(
+      "https://rosetest.library.jhu.edu/rosademo/wa/rose/SeldenSupra57/1r/canvas",
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json"
+        }
+      }
+    ).then(response => {
+      if (response.ok) {
+        response.json().then(json => {
+          this.setState({ responseText: json });
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>
+          {typeof this.state.responseText["@context"] !== "undefined" ? (
+            Object.entries(this.state.responseText["@context"]).map(
+              ([key, value]) => {
+                return (
+                  <li key={`${key}`}>
+                    <p style={{ margin: `0px 10px 0px 2vw` }}>{value}</p>
+                  </li>
+                );
+              }
+            )
+          ) : (
+            <></>
+          )}
+        </p>
+      </div>
+    );
+  }
+}
 
 export default {
   name: "Demo",
-  target: "WindowTopBarPluginMenu",
-  mode: "add",
+  target: "WindowSideBarInfoPanel",
+  mode: "wrap",
   component: DemoComponent
 };
