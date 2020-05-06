fa32b63012c0f41be7f0edf10db95b0e1a390e7f
GroovinChip <groovinchip@gmail.com>
2020-05-06 11:24:22 -0400

    Styling updates for custom annotation panel
    Removed 'label' string; Italicized annotation part labels; tab indent annotation part values; annotation part values on newline

--- src/plugins/jh-annotations-panel/components/annotation.js
+++ src/plugins/jh-annotations-panel/components/annotation.js
@@ -3,7 +3,7 @@ import AnnotationBody from './annotationBody';
 
 /**
  * Should display an annotation, possibly decorated by other annotations
- * 
+ *
  * props: {
  *    annotation: { ... }, // The subject of this component
  *    targetedBy: [
@@ -12,13 +12,14 @@ import AnnotationBody from './annotationBody';
  *  }
  */
 export default class Annotation extends Component {
-
   render() {
     const { annotation } = this.props;
 
     // The annotation has content if 'annotation.body' is an array with 1 or more elements,
     // or is not an array and exists
-    const hasContent = Array.isArray(annotation.body) ? annotation.body.length > 0 : !!annotation.body;
+    const hasContent = Array.isArray(annotation.body)
+      ? annotation.body.length > 0
+      : !!annotation.body;
 
     // If no content is detected, return early
     if (!hasContent) {
@@ -32,23 +33,30 @@ export default class Annotation extends Component {
     let creator;
     if (annotation.creator) {
       if (typeof annotation.creator === 'string') {
-        creator = (<><span>Creator: </span><a href={annotation.creator} target="_blank">{annotation.creator}</a></>);
+        creator = (
+          <>
+            <span style={{ fontStyle: 'italic' }}>Creator: </span>
+            <a href={annotation.creator} target='_blank'>
+              {annotation.creator}
+            </a>
+          </>
+        );
       } else if (typeof annotation.creator === 'object') {
-        creator = (<a href={annotation.creator.id} target="_blank">{annotation.creator.name}</a>);
+        creator = (
+          <a href={annotation.creator.id} target='_blank'>
+            {annotation.creator.name}
+          </a>
+        );
       }
     } else {
       creator = <></>;
     }
-    
+
     return (
-      <div className="annotation">
-        <div className="annotation-label">Label: {annotation.label}</div>
-        <div className="annotation-bodies">
-          {bodies}
-        </div>
-        <div className="annotation-creator">
-          {creator}
-        </div>
+      <div className='annotation'>
+        <div className='annotation-label'>{annotation.label}</div>
+        <div className='annotation-bodies'>{bodies}</div>
+        <div className='annotation-creator'>{creator}</div>
       </div>
     );
   }
