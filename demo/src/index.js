24d0f0f05d3f14873764af585e668d25217cd855
GroovinChip <groovinchip@gmail.com>
2020-05-08 12:35:40 -0400

    Update theme

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,19 +1,19 @@
 import Mirador from 'mirador';
-import { 
-  jhAnnotationFetchPlugin,
-  jhAnnotationsPanelPlugin
-} from '../../src';
+import { jhAnnotationFetchPlugin, jhAnnotationsPanelPlugin } from '../../src';
 
 const config = {
   id: 'demo',
   windows: [
     {
-      loadedManifest: 'https://rosetest.library.jhu.edu/rosademo/iiif/homer/VA/manifest',
-      canvasId: 'https://rosetest.library.jhu.edu/rosademo/iiif/homer/VA/VA035RN-0036/canvas'
+      loadedManifest:
+        'https://rosetest.library.jhu.edu/rosademo/iiif/homer/VA/manifest',
+      canvasId:
+        'https://rosetest.library.jhu.edu/rosademo/iiif/homer/VA/VA035RN-0036/canvas',
     },
     {
-      loadedManifest: 'https://rosetest.library.jhu.edu/rosademo/iiif/rose/SeldenSupra57/manifest'
-    }
+      loadedManifest:
+        'https://rosetest.library.jhu.edu/rosademo/iiif/rose/SeldenSupra57/manifest',
+    },
   ],
   window: {
     defaultView: 'single',
@@ -27,23 +27,37 @@ const config = {
     'https://rosetest.library.jhu.edu/rosademo/iiif/rose/Morgan948/manifest': {},
     'https://rosetest.library.jhu.edu/rosademo/iiif/rose/SeldenSupra57/manifest': {},
     'https://rosetest.library.jhu.edu/rosademo/iiif/rose/Walters143/manifest': {},
-    'https://rosetest.library.jhu.edu/rosademo/iiif/homer/VA/manifest': {}
+    'https://rosetest.library.jhu.edu/rosademo/iiif/homer/VA/manifest': {},
   },
   thumbnailNavigation: {
-    defaultPosition: 'far-bottom'
+    defaultPosition: 'far-bottom',
   },
-  theme: {
-    palette: {
-      primary: {
-        main: '#1967d2'
-      }
-    }
-  }
+  themes: {
+    dark: {
+      palette: {
+        type: 'dark',
+        primary: {
+          main: '#3F51B5',
+        },
+        secondary: {
+          main: '#536dfe',
+        },
+      },
+    },
+    light: {
+      palette: {
+        type: 'light',
+        primary: {
+          main: '#3F51B5',
+        },
+        secondary: {
+          main: '#536dfe',
+        },
+      },
+    },
+  },
 };
 
-const plugins = [
-  jhAnnotationFetchPlugin,
-  jhAnnotationsPanelPlugin
-];
+const plugins = [jhAnnotationFetchPlugin, jhAnnotationsPanelPlugin];
 
 const miradorInstance = Mirador.viewer(config, plugins);
