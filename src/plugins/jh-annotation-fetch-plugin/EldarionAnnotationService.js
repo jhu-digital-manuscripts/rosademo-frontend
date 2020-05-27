85032cfabe8a434955fa21114179999951188617
John Abrahams <jabrah23@gmail.com>
2020-05-27 13:02:00 -0400

    Should use an 'Eldarion discovery service' if one exists on a manifest

--- src/plugins/jh-annotation-fetch-plugin/EldarionAnnotationService.js
+++ src/plugins/jh-annotation-fetch-plugin/EldarionAnnotationService.js
@@ -4,13 +4,17 @@ import {
   jsonify
 } from './webAnnotationUtils';
 
+const _EDS_PROFILE_URL = 'https://eldarion.com/wa/discovery/';
+
 /**
- * Inteded to be used in development only.
+ * If an Eldarion Discovery Service (EDS) exists for the given manifest, use it to try to find
+ * Eldarion web annotations for the canvases.
  *
  * @param {array} canvases - array of Canvas objects
  * @param {function} receiveAnnotation - callback function to stuff the fetched annotations into
+ * @param {Manifest} manifest - IIIF Manifest object
  */
-export function getEldarionAnnotations(canvases, receiveAnnotation) {
+export function getEldarionAnnotations(canvases, receiveAnnotation, manifest) {
   if (!canvases) {
     return;
   }
@@ -26,7 +30,7 @@ export function getEldarionAnnotations(canvases, receiveAnnotation) {
      * New section will try a known URL to reach the trial discovery service in order to
      * get annotation endpoints
      */
-    const discoveryUrl = 'https://aniop-atlas-staging.eldarion.com/wa/discovery/';
+    const discoveryUrl = getEDS(manifest) || 'https://aniop-atlas-staging.eldarion.com/wa/discovery/';
     const target = `${discoveryUrl}?canvas_id=${canvasId}`;
 
     fetch(target, { method: 'GET' })
@@ -51,6 +55,34 @@ export function getEldarionAnnotations(canvases, receiveAnnotation) {
       })
       .catch(error => console.log(`${target} : ${error}`));
   });
+}
+
+/**
+ * Get the Eldarion Discovery Service, if one is associated with this manifest. Return 'undefined'
+ * if none is found. This plugin inherently knows how to interpret this service.
+ *
+ * @param {Manifest} manifest IIIF Manifest json data
+ * @returns IIIF Service block {
+ *    '@context: ''
+ *    '@id': '',
+ *    'profile': '',
+ *    'label: ''
+ * }
+ */
+function getEDS(manifest) {
+  console.assert(!!manifest, 'No manifest provided');
+
+  if (!manifest) {
+    return;
+  }
+  
+  const services = manifest.service;
+
+  if (!Array.isArray(services)) {
+    services = [services];
+  }
+
+  return services.find(service => service?.profile === _EDS_PROFILE_URL);
 }
 
 /**
