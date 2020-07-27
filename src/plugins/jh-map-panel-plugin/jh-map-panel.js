5333be842a923a3fe5ec898778db9f2414aaf5ba
Reuben Turner <groovinchip@gmail.com>
2020-07-27 14:22:41 -0400

    Placeholder map dialog
    Had to switch to function component to make it work

--- src/plugins/jh-map-panel-plugin/jh-map-panel.js
+++ src/plugins/jh-map-panel-plugin/jh-map-panel.js
@@ -1,33 +1,97 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import CompanionWindow from 'mirador/dist/es/src/containers/CompanionWindow';
 import ns from 'mirador/dist/es/src/config/css-ns';
-import { Typography } from '@material-ui/core';
+import { Button, Dialog, DialogTitle, DialogContent } from '@material-ui/core';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
-export default class JHMapPanel extends Component {
-  render() {
-    const { classes, windowId, id } = this.props;
+function MapDialog(props) {
+  const { onClose, open } = props;
 
-    return (
-      <CompanionWindow
-        title='Locations'
-        paperClassName={ns('window-sidebar-map-panel')}
-        windowId={windowId}
-        id={id}
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <Dialog
+      onClose={handleClose}
+      aria-labelledby='simple-dialog-title'
+      open={open}
+      maxWidth='lg'
+      fullWidth={true}
+    >
+      <DialogTitle id='simple-dialog-title'>Locations</DialogTitle>
+      <DialogContent>
+        <Map
+          scrollWheelZoom={false}
+          touchZoom={false}
+          style={{ height: '750px' }}
+        >
+          <TileLayer
+            //attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
+        </Map>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+MapDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+};
+
+export default function JHMapPanel(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const { classes, windowId, id } = props;
+
+  return (
+    <CompanionWindow
+      title='Locations'
+      paperClassName={ns('window-sidebar-map-panel')}
+      windowId={windowId}
+      id={id}
+    >
+      <div>
+        <Map
+          scrollWheelZoom={false}
+          touchZoom={false}
+          style={{ height: '250px' }}
+        >
+          <TileLayer
+            //attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
+        </Map>
+      </div>
+      <div
+        id='mapPanelExpandButton'
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          margin: '8px',
+        }}
       >
-        <div>
-          <Map
-            scrollWheelZoom={false}
-            touchZoom={false}
-            style={{ height: '250px' }}
-          >
-            <TileLayer
-              //attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-            />
-          </Map>
-        </div>
-      </CompanionWindow>
-    );
-  }
+        <Button
+          variant='contained'
+          color='primary'
+          size='small'
+          onClick={handleClickOpen}
+        >
+          Enlarge Map
+        </Button>
+        <MapDialog open={open} onClose={handleClose} />
+      </div>
+    </CompanionWindow>
+  );
 }
